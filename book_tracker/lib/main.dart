761b6ff6335f302a2d8edc56f2246923e3d2ddc1
pdichone <pdichone@gmail.com>
2021-03-24 15:45:14 -0700

    update main.dart
    testing

--- book_tracker/lib/main.dart
+++ book_tracker/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Book Tracker',
+      title: 'Book Tracker web apps',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
