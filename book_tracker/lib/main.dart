87542c556d535295ac597d264cde049a5ba7e05b
pdichone <pdichone@gmail.com>
2021-03-24 19:39:43 -0700

    refactor
    Refactor

--- book_tracker/lib/main.dart
+++ book_tracker/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:book_tracker/views/main_body.dart';
+import 'package:book_tracker/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,49 +16,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 2,
-        centerTitle: true,
-        title: Text(
-          "Book Tracker",
-          style: TextStyle(
-              color: Colors.black,
-              fontSize: 24.0,
-              fontWeight: FontWeight.w500,
-              fontStyle: FontStyle.italic),
-        ),
-        leading: Padding(
-          padding: const EdgeInsets.all(8),
-        ),
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: TextButton.icon(
-              label: Text('Login'),
-              icon: Icon(Icons.login_rounded),
-              onPressed: () {},
-            ),
-          )
-        ],
-      ),
-      body: Stack(
-        children: [
-          // Social Buttons
-          MainBody(),
-
-          // Body
-        ],
-      ),
     );
   }
 }
